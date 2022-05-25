249273563e85b0ad1db56d4f33ed17602f136715
AdamRathbun <hesle1119@gmail.com>
2022-05-25 09:02:06 -0400

    31

---
+++ function withoutLast(arr) {   return arr.slice(0,(arr.length-1)) }  :* P array R array without last element E function([1,43,2,12]) returns [1.43.2] P if pop() is bad, could use slice() to not change original array  *:.js
@@ -0,0 +1,23 @@
+function pipeFix(numbers){
+    let arr=[]
+    numbers.sort((a,b)=>a-b)
+    let x=numbers[0]
+    let y=numbers[numbers.length-1]
+    for (let i=x; i<=y; i++){
+      arr.push(i)
+    }
+    console.log(arr)
+    return arr
+  }
+  
+  
+  /*
+  P: array of nums
+  R: array with every integer from the lowest to the highest integer of the original array
+  E: function([1,5,3]) returns [1,2,3,4,5]
+  P: 
+  sort 
+  assign lowest and highest
+  for loop lowest to highest push to empty array
+  
+  */
