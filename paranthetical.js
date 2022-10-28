a224f4983fd03998e594fbb9963f958976522544
AdamRathbun <hesle1119@gmail.com>
2022-10-28 17:17:14 -0400

    t146

--- paranthetical.js
+++ paranthetical.js
@@ -9,3 +9,6 @@
 function paranthetical(string){
     return string.split('').map(x=> string.indexOf(x.toLowerCase())=== string.lastIndexOf(x.toLowerCase()) ? x='(' : x=')').join('')
 }
+// ^this is quadratic - O(n^2), b.c map + indexOf/lastIndexOf
+
+// the better way to do this would be caching, making an object of each letter (hash), then count it. It's O(2N), which becomes O(N)
