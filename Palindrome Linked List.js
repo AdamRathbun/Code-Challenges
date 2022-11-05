d5624bf14d4a325a31b0f146590842b22b26b18b
AdamRathbun <hesle1119@gmail.com>
2022-11-05 16:46:53 -0400

    t151

---
+++ Palindrome Linked List.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    let str =''
+    while (head){
+        str+=head.val
+        head=head.next
+    }
+    const reverse = str.split('').reverse().join('')
+    return str === reverse
+};
