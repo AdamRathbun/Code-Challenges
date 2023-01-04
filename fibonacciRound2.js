2aa0ceb75a24a383303dec60069c13eae8c8427a
AdamRathbun <hesle1119@gmail.com>
2023-01-04 11:15:23 -0500

    t190

--- fibonacciRound2.js
+++ fibonacciRound2.js
@@ -1,10 +1,10 @@
-function fibonacci2(num){
-    let arr = [0,1]
-    for (let i=2; i<=num; i++){
-        arr[i] = arr[i-1] + arr[i+2]
-    }
-    return arr[num]
-}
+// let arr = [0,1]
+// function fibonacci2(num){
+//     for (let i=2; i<=num; i++){
+//         arr[i] = arr[i-1] + arr[i-2]
+//     }
+//     return arr[num]
+// }
 
 function fibonacci3(num){
     if (num<2){
@@ -19,31 +19,27 @@ let memo = {}
 const fib = function(n){
     if (n === 1 || n === 0){
         return n
+        // time complexity = O(1)( memo2[n]!==null ){
     }else if (memo[n]){
         return memo[n]
+        // time complexity = O(1)
     }else {
         let result = fib(n-1) + fib(n-2)
         memo[n] = result
-        return result
-    }
-}
-
-
-// slightly refactored:
-let memo2 = []
-const fib2 = function (n, memo2){
-    if ( memo2[n]!==null ){
-        return memo[n]
-        // time complexity = O(1)
-    }else if (n ===1 || n===2){
-        return n
-        // time complexity = O(1)
-    }else {
-        result = fib2[n-1] + fib2[n-2]
-        memo2[n] = result
         return result
         // time complexity = O(2n+1)
     }
 }
 
 // note the time complexity of memoization is rounded to O(n)
+
+
+// bottom up approach:
+let arr = [0,1]
+const fibBottomUp = function(num){
+    for (let i=2; i<=num; i++){
+        arr[i] = arr[i-1] + arr[i-2]
+    }
+    return arr[num]
+}
+// time complexity is O(n)
