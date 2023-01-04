63aacb03706b1ea94885d56587ad2518fc3a266d
AdamRathbun <hesle1119@gmail.com>
2023-01-04 10:28:38 -0500

    t189

--- fibonacciRound2.js
+++ fibonacciRound2.js
@@ -13,3 +13,17 @@ function fibonacci3(num){
         return (fibonacci3(num-1) + fibonacci3(num-2))
     }
 }
+
+// memoization: an optimization technique that makes applications more efficient and hence faster. It does this by storing computation results in cache, and retrieving that info from the cache the next time it's needed instead of computing it again.
+let memo = {}
+const fib = function(n){
+    if (n === 1 || n === 0){
+        return n
+    }else if (memo[n]){
+        return memo[n]
+    }else {
+        let number = fib(n-1) + fib(n-2)
+        memo[n] = number
+        return number
+    }
+}
