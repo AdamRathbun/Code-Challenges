3ab9382ce48668d834ca87fe3fd9051f2e3036a6
AdamRathbun <hesle1119@gmail.com>
2023-01-18 18:14:25 -0500

    t192

--- factorial.js
+++ factorial.js
@@ -12,9 +12,15 @@
 
 
 function factorial(n){
-    if (n === 1 || n === 0){
+    if (n < 2){
         return 1
     }else {
         return n * factorial(n-1)
     }
 }
+
+// 5* factorial(5-1)
+// 4* factorial(4-1)
+// 3* factorial(3-1)
+// 2* factorial(2-1)
+// 1
