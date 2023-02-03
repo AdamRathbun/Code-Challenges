ce2c153c75c92a8835b1f4aca73dc3a594e786cb
AdamRathbun <hesle1119@gmail.com>
2023-02-03 18:13:20 -0500

    t197

--- queue.js
+++ queue.js
@@ -1,3 +1,34 @@
+//   Queue, the good version
+
+class Queue{
+    constructor(){
+      this.storage={}
+      this.head=0
+      this.tail=0
+    }
+    enqueue(item){
+      this.storage[this.tail]=item
+      this.tail++
+    }
+    dequeue(){
+      const removed = this.storage[this.head]
+      delete this.storage[this.head]
+      this.head++
+      return removed
+    }
+
+  }
+
+  const queue = new Queue()
+  queue.enqueue('seahorse')
+  queue.enqueue('shark')
+  queue.enqueue('turtle')
+  queue.dequeue()
+
+//   Object.keys(queue.storage) returns ['1', '2']
+//   queue.storage['1'] returns 'shark'
+
+// Queue - bad version
 class Queue{
     constructor(){
       this.array=[]
@@ -7,8 +38,40 @@ class Queue{
     }
     dequeue(){
       return this.array.shift()
+    //   bad because shift() is O(n) since you're shifting the entire array
     }
     size(){
       return this.array.length
     }
+    peek(){
+      return this.array[0];
+    }
   }
+
+//   Stack
+  class Stack{
+    constructor(){
+        this.storage={}
+        this.size=0
+    }
+    push(item){
+        this.size++
+        this.storage[this.size]=item
+    }
+    pop(){
+        const removed = this.storage[this.size]
+        delete this.storage[this.size]
+        this.size--
+        return removed
+    }
+    peek(){
+        return this.storage[this.size]
+    }
+  }
+
+  const stack = new Stack()
+  stack.push('dog')
+  stack.push('cat')
+  stack.push('bear')
+  stack.pop()
+  stack.peek()
