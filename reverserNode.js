4c3db2cc25d8c4fb8ca7fd4b5008ee3bbc5cae5e
AdamRathbun <hesle1119@gmail.com>
2023-03-02 21:45:07 -0500

    t206

--- reverserNode.js
+++ reverserNode.js
@@ -18,3 +18,11 @@ function reverserNode(head){
     }
     return prev
 }
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+reverserNode(head)
