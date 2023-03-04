15569415afa92289950767014e51c5fd3f4bcc5f
AdamRathbun <hesle1119@gmail.com>
2023-03-03 21:45:01 -0500

    t207

--- reverserNode.js
+++ reverserNode.js
@@ -26,3 +26,34 @@ head.next.next.next = new Node(4);
 head.next.next.next.next = new Node(5);
 
 reverserNode(head)
+
+
+
+
+
+
+class Node{
+    constructor(value, next){
+        this.value=value
+        this.next=next || null
+    }
+}
+
+function reverser(head){
+    let current=head
+    let prev=null
+    while(current){
+        let next=current.next
+        current.next=prev
+        prev=current
+        current=next
+    }
+    return prev
+}
+
+let myNode=new Node(1)
+myNode.next=new Node(2)
+myNode.next.next=new Node(3)
+myNode.next.next.next=new Node(4)
+myNode.next.next.next.next=new Node(5)
+reverser(myNode)
